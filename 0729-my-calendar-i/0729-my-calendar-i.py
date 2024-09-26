f89c679e8303d50482c218e75ce4fd13574f1441
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-09-25 21:53:58 -0400

    Time: 313 ms (37.53%), Space: 17.5 MB (24.81%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -11,8 +11,8 @@ class MyCalendar:
             if e <= start:
                 continue
             return False
-        self.intervals.append((start, end))
-        self.intervals.sort()
+        index = bisect_left(self.intervals, end, 0, len(self.intervals), key=lambda x: x[0])
+        self.intervals.insert(index, (start, end))
         return True
 
 # Your MyCalendar object will be instantiated and called as such:
