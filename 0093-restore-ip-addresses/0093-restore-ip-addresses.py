ea0432235aef2bc57dc1f096e11a9171113f577b
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-10-03 23:03:48 -0400

    Time: 63 ms (9.4%), Space: 16.7 MB (5.94%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        result = []
+        cur = []
+        def backtrack(x):
+            if x==len(s) and len(cur)==4:
+                result.append(".".join(cur))
+                return
+            # get the numbers which are in the range 0 to 255
+            for i in range(3):
+                if x+i+1<=len(s) and 0<=int(s[x:x+i+1])<=255:
+                    if i>=1 and s[x:x+i+1].startswith("0"):
+                        continue
+                    cur.append(s[x:x+i+1])
+                    backtrack(x+i+1)
+                    cur.pop()
+        backtrack(0)
+        return result
