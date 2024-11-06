422a9aeba993ee0ee170cca11a7e44a473a465cd
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-11-06 02:20:36 -0500

    Time: 0 ms (100%), Space: 17.3 MB (92.77%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -3,42 +3,42 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-def reverseKNodes(node, k):
-    a, b = node, node.next
-    cur_k=1
-    node.next=None
-    while b and cur_k!=k:
-        c=b.next
-        b.next=a
-        a=b
-        b=c
-        cur_k+=1
-    return a
-
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = ListNode()
-        dummy.next = head
-        start = head
-        ptr = head
-        cur_k = 1
-        endG = None
-        while ptr:
+        def reverseKNodes(node, end):
+            a=node
+            b=node.next
+            a.next=None
+            while a!=end:
+                c=b.next
+                b.next=a
+                a=b
+                b=c
+            return a
+
+        dummy=ListNode()
+        dummy.next=None
+        cur_k=1
+        curGroupStart=head
+        prevGroupEnd=None
+        while head:
             if not cur_k%k:
-                nextGstart = ptr.next
-                reversedG = reverseKNodes(start, k)
-                if endG:
-                    endG.next=reversedG
-                else:
-                    dummy.next=reversedG
-                endG = start
-                start = nextGstart
-                ptr=nextGstart
+                curGroupEnd=head
+                head=head.next
+                curGroupEnd.next=None
+                #reverse the nodes
+                groupStart=reverseKNodes(curGroupStart, curGroupEnd)
+                if not dummy.next:
+                    dummy.next=groupStart
+                if prevGroupEnd:
+                    prevGroupEnd.next=groupStart
+                prevGroupEnd=curGroupStart
+                curGroupStart=head
                 cur_k+=1
                 continue
-            ptr=ptr.next
             cur_k+=1
-        if nextGstart:
-            endG.next=nextGstart
+            head=head.next
+        if prevGroupEnd:
+            prevGroupEnd.next=curGroupStart
         return dummy.next
-
+        
