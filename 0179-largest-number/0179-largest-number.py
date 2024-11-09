0cfce28467c5f0d9380cc7d66556d689471411a6
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-11-08 23:27:11 -0500

    Time: 2 ms (57.34%), Space: 16.7 MB (18.76%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,17 +1,9 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        nums = list(map(str, nums))
-        count = collections.Counter(nums)
-        keys = list(count.keys())
-        keys.sort(reverse=True)
-        # go through nums and check if the numbers have the same prefix and length if diff
-        for i, j in zip(range(len(keys)-1), range(1, len(keys))):
-            while i>=0 and j>=0 and keys[i].startswith(keys[j]) and len(keys[i])>len(keys[j]) and keys[j]+keys[i]>keys[i]+keys[j]:
-                keys[j], keys[i] = keys[i], keys[j]
-                i-=1
-                j-=1
-        result = []
-        for k in keys:
-            result = result + ([k]*count[k])
-        return str(int("".join(result)))
-        
+        nums=list(map(str, nums))
+        def cmp(n1, n2):
+            if n2+n1>n1+n2:
+                return 1
+            return -1
+        nums.sort(key=cmp_to_key(cmp))
+        return str(int("".join(nums)))
