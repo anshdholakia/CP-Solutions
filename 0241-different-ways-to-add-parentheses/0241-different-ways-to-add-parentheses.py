396d6fdafb2723f5011b631cc68447ac7fe59af5
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-11-10 00:42:15 -0500

    Time: 3 ms (54.9%), Space: 16.9 MB (15.3%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,37 +1,16 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        result = []
-        s_exp = []
-        l = 0
-        for i in range(len(expression)):
-            if expression[i] in '+-*':
-                s_exp.append(expression[l:i])
-                s_exp.append(expression[i])
-                l=i+1
-        s_exp.append(expression[l:i+1])
-        def dnc(cur_stack):
-            if len(cur_stack)>1:
-                result = []
-                for i in range(len(cur_stack)):
-                    if cur_stack[i] == '+':
-                        res1=dnc(cur_stack[:i])
-                        res2=dnc(cur_stack[i+1:])
-                        for r1 in res1:
-                            for r2 in res2:
-                                result.append(r1+r2)
-                    elif cur_stack[i]=='-':
-                        res1=dnc(cur_stack[:i])
-                        res2=dnc(cur_stack[i+1:])
-                        for r1 in res1:
-                            for r2 in res2:
-                                result.append(r1-r2)
-                    elif cur_stack[i]=='*':
-                        res1=dnc(cur_stack[:i])
-                        res2=dnc(cur_stack[i+1:])
-                        for r1 in res1:
-                            for r2 in res2:
-                                result.append(r1*r2)
-                return result
-            else:
-                return [int(cur_stack[0])]
-        return dnc(s_exp)
+        operator={'+':lambda n1,n2:n1+n2, '-':lambda n1,n2:n1-n2, '*':lambda n1,n2:n1*n2}
+        def backtrack(left, right):
+            res=[]
+            for i in range(left, right+1):
+                if expression[i] in '*+-':
+                    nums1=backtrack(left, i-1)
+                    nums2=backtrack(i+1, right)
+                    for n1 in nums1:
+                        for n2 in nums2:
+                            res.append(operator[expression[i]](n1, n2))
+            if not res:
+                return [int(expression[left:right+1])]
+            return res
+        return backtrack(0, len(expression)-1)
