8dd7972e5ee3459411df08f02a7ada983d6a8bc8
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-11-10 18:42:29 -0500

    Time: 3 ms (42.3%), Space: 17.1 MB (13.83%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,37 +1,44 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        adj = {char: set() for word in words for char in word}
-        for i in range(len(words) - 1):
-            w1, w2 = words[i], words[i + 1]
-            minLen = min(len(w1), len(w2))
-            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
-                return ""
-            for j in range(minLen):
-                if w1[j] != w2[j]:
-                    adj[w1[j]].add(w2[j])
-                    break
-        print(adj)
-        topo = []
-        visited = set({})
-        cycle = set({})
-        def dfs(node):
-            if node in cycle:
+        G=defaultdict(set)
+        # filter the words out
+        newwords=[]
+        newwords.append(words[0])
+        for i in range(1, len(words)):
+            if words[i-1]==words[i]:
+                continue
+            newwords.append(words[i])
+        words=newwords
+        for w1 in range(len(words)):
+            for c in words[w1]:
+                G[c]
+            for w2 in range(w1+1, len(words)):
+                if (words[w2].startswith(words[w1]) or words[w1].startswith(words[w2])) and len(words[w1])>len(words[w2]):
+                    return ""
+                i, j = 0, 0
+                while i<len(words[w1]) and j<len(words[w2]) and words[w1][i]==words[w2][j]:
+                    i+=1
+                    j+=1
+                if i<len(words[w1]) and j<len(words[w2]):
+                    G[words[w1][i]].add(words[w2][j])
+        # topo sort
+        visited=set({})
+        cycle=set({})
+        res=[]
+        def dfs(letter):
+            if letter in cycle:
                 return False
-            if node in visited:
+            if letter in visited:
                 return True
-            visited.add(node)
-            if not adj[node]:
-                topo.append(node)
-                return True
-            cycle.add(node)
-            for n in adj[node]:
-                if not dfs(n):
+            cycle.add(letter)
+            for neighbor in G[letter]:
+                if not dfs(neighbor):
                     return False
-            cycle.remove(node)
-            topo.append(node)
+            cycle.remove(letter)
+            visited.add(letter)
+            res.append(letter)
             return True
-        for w in adj:
-            if w not in visited and not dfs(w):
+        for key in G.copy():
+            if not dfs(key):
                 return ""
-        return "".join(topo[::-1])
-            
+        return "".join(res[::-1])
