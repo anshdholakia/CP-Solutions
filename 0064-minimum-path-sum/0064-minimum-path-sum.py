095e3890b19032d9bc29daf73b348455cf4ef593
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-11-11 01:28:46 -0500

    Time: 19 ms (39.46%), Space: 18.9 MB (44.16%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,13 +1,12 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        # dijkstra
-        visited = set({(0, 0)})
-        minheap=[(grid[0][0], 0, 0)]
-        while minheap:
-            val, x, y = heapq.heappop(minheap)
-            if x==len(grid)-1 and y==len(grid[0])-1:
-                return val
-            for dx, dy in [(0, 1), (1, 0)]:
-                if 0<=dx+x<len(grid) and 0<=dy+y<len(grid[0]) and (dx+x, dy+y) not in visited:
-                    visited.add((dx+x, dy+y))
-                    heapq.heappush(minheap, (val+grid[dx+x][dy+y], dx+x, dy+y))
+        # this is simple 2d dp
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                opt1, opt2 = inf, inf
+                if i>0:
+                    opt1=grid[i-1][j]
+                if j>0:
+                    opt2=grid[i][j-1]
+                grid[i][j]=min(grid[i][j]+opt1, grid[i][j]+opt2) if min(opt1, opt2)!=inf else grid[i][j]
+        return grid[-1][-1]
