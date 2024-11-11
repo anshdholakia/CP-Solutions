95264a601f3b0182a965779e44d986e6345dfd1a
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-11-11 01:36:23 -0500

    Time: 0 ms (100%), Space: 16.7 MB (22.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,31 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # swap all 0s and 2s
-        l, r = 0, len(nums)-1
-        while l<r:
-            if nums[r]==0 and nums[l]==2:
-                nums[r], nums[l] = nums[l], nums[r]
-            if nums[r]!=0:
-                r-=1
-            if nums[l]!=2:
-                l+=1
-        # swap all 0s and 1s
-        l, r = 0, len(nums)-1
-        while l<r:
-            if nums[r]==0 and nums[l]==1:
-                nums[r], nums[l] = nums[l], nums[r]
-            if nums[r]!=0:
-                r-=1
-            if nums[l]!=1:
-                l+=1
-        # swap all 2s and 1s
-        l, r = 0, len(nums)-1
-        while l<r:
-            if nums[r]==1 and nums[l]==2:
-                nums[r], nums[l] = nums[l], nums[r]
-            if nums[r]!=1:
-                r-=1
-            if nums[l]!=2:
-                l+=1
-        return nums
+        # quick select algorithm
+        def quick_select(i, j, swp1, swp2):
+            while i<j:
+                while i<j and nums[i]!=swp2:
+                    i+=1
+                while i<j and nums[j]!=swp1:
+                    j-=1
+                if i<j:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i+=1
+                    j-=1
+        quick_select(0, len(nums)-1, 0, 2)
+        quick_select(0, len(nums)-1, 0, 1)
+        quick_select(0, len(nums)-1, 1, 2)
+        
+            
