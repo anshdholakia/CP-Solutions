189ebe3638fb97fa1396798e15c48690277af499
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-11-12 15:53:34 -0500

    Time: 75 ms (49.8%), Space: 31.2 MB (17.05%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
@@ -1,16 +1,13 @@
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        NEED = len(arr)//2
-        mod_to_n = {}
-        cur = 0
+        ps=defaultdict(int) # store the modulo with k
         for n in arr:
-            rem = n%k
-            if rem<0:
-                rem+=k
-            mod_to_n[rem] = mod_to_n.get(rem, 0)+1
-        if mod_to_n.get(0, 0)%2:
-            return False
-        for i in range(1, k//2+1):
-            if mod_to_n.get(i, 0) != mod_to_n.get(k-i, 0):
+            ps[n%k]+=1
+        print(ps)
+        for n in ps:
+            if n==0:
+                if ps[n]%2!=0:
+                    return False
+            elif ps[k-n]!=ps[n]:
                 return False
         return True
