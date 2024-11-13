232501cf47e7f64315e52adf11682630d141e380
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-11-12 19:58:06 -0500

    Time: 57 ms (48.13%), Space: 19.2 MB (99.52%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        G = defaultdict(list)
+        for fromm, to in prerequisites:
+            G[fromm].append(to)
+        nodeToChildren=defaultdict(set) # node value: set of children
+        # nodeToChildren={1: set(0, 2), 2: set(0), 0: set()}
+        # BFS on EACH NODE
+        def bfs(node):
+            queue=collections.deque([node])
+            visited=set({})
+            while queue:
+                pop=queue.popleft()
+                for neighbor in G[pop]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append(neighbor)
+            nodeToChildren[node].update(visited)
+        for i in range(numCourses):
+            bfs(i)
+        res=[]
+        for a, b in queries:
+            if b in nodeToChildren[a]:
+                res.append(True)
+            else:
+                res.append(False)
+        return res
