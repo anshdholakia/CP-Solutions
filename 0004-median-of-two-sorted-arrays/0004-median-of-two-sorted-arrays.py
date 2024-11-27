09e6f2e54413896a7042c3dce6426fbca23fa6d8
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-11-27 01:51:52 -0500

    Time: 0 ms (100%), Space: 17.8 MB (5.39%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,26 +1,25 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1)<len(nums2):
-            nums1, nums2 = nums2, nums1
-        if not nums2:
+        # use binary search
+        if not nums1 or not nums2:
+            nums1+=nums2
             if len(nums1)%2:
                 return nums1[len(nums1)//2]
-            return (nums1[len(nums1)//2]+nums1[len(nums1)//2-1])/2.0
-        TOTAL = len(nums1)+len(nums2)
-        HALF = TOTAL//2
-        l, r = 0, len(nums2)-1
+            return (nums1[len(nums1)//2]+nums1[(len(nums1)//2)-1])/2
+        if len(nums1)>len(nums2):
+            nums1, nums2 = nums2, nums1
+        TOTAL=len(nums1)+len(nums2)
+        HALF=TOTAL//2
+        l, r = 0, len(nums1)-1
         while True:
-            m = (l+r)//2
-            m2 = HALF-m-1
-            L2 = nums2[m] if m>=0 else -float("inf")
-            R2 = nums2[m+1] if m+1<len(nums2) else float("inf")
-            L1 = nums1[m2-1] if len(nums1)>m2-1>=0 else -float("inf")
-            R1 = nums1[m2] if 0<=m2<len(nums1) else float("inf")
-            if L2<=R1 and L1<=R2:
+            m=(l+r)//2
+            LOWER1, UPPER1 = nums1[m] if 0<=m<len(nums1) else -inf, nums1[m+1] if 0<=m+1<len(nums1) else inf
+            LOWER2, UPPER2 = nums2[HALF-m-2] if 0<=HALF-m-2<len(nums2) else -inf, nums2[HALF-m-1] if 0<=HALF-m-1<len(nums2) else inf
+            if LOWER1<=UPPER2 and LOWER2<=UPPER1:
                 if TOTAL%2:
-                    return min(R1, R2)
-                return (max(L1, L2)+min(R1, R2))/2.0
-            if L2>R1:
+                    return min(UPPER1, UPPER2)
+                return (min(UPPER1, UPPER2)+max(LOWER1, LOWER2))/2
+            if LOWER1>UPPER2:
                 r=m-1
             else:
                 l=m+1
