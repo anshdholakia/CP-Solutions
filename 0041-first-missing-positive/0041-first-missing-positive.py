fe06b2e3aa38c7d27dbc928ba5f445dcb393a00b
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-12-02 13:49:26 -0500

    Time: 78 ms (8.22%), Space: 28.6 MB (50.35%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,20 +1,18 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # put all negatives as zeros
-        for i in range(len(nums)):
-            if nums[i]==0:
-                nums[i]=len(nums)+1
-            elif nums[i]<0:
+        # use cyclic sort
+        # replace all negatives with zeros
+        n=len(nums)
+        for i, x in enumerate(nums):
+            if x<0:
                 nums[i]=0
-        # mark all indices here
-        for n in nums:
-            if 0<=abs(n)-1<len(nums):
-                if nums[abs(n)-1]>0:
-                    nums[abs(n)-1]*=-1
-                elif nums[abs(n)-1]==0:
-                    nums[abs(n)-1]=-(len(nums)+1)
-        # check closest n
-        for n in range(1, len(nums)+1):
-            if nums[n-1]>=0:
-                return n
+        for x in range(len(nums)):
+            if nums[x]!=0 and abs(nums[x])-1<n:
+                if nums[abs(nums[x])-1]>0:
+                    nums[abs(nums[x])-1]*=-1
+                elif nums[abs(nums[x])-1]==0:
+                    nums[abs(nums[x])-1]=-n
+        for i in range(n):
+            if nums[i]>=0:
+                return i+1
         return n+1
