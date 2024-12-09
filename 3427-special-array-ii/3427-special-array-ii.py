60f596653f2e13b8c055c1afbc3894db6175177e
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2024-12-08 22:45:33 -0500

    Time: 34 ms (87.5%), Space: 46.7 MB (42.51%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.py
+++ 3427-special-array-ii/3427-special-array-ii.py
@@ -1,20 +1,15 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        result=[False]*len(queries)
-        queries=[[t[0], t[1], i] for i, t in enumerate(queries)]
-        queries.sort(key=lambda x: (x[0], x[1]))
-        # minheap stores end, index of query
-        minheap=[]
-        idx=0
-        latest_hiccup=None
-        for i in range(len(nums)):
-            while idx<len(queries) and queries[idx][0]==i:
-                heapq.heappush(minheap, (queries[idx][1], queries[idx][0], queries[idx][2]))
-                idx+=1
-            while minheap and minheap[0][0]==i:
-                pope, pops, popi = heapq.heappop(minheap)
-                if latest_hiccup!=None:
-                    result[popi] = pops<=latest_hiccup<=pope
-            if i+1<len(nums) and ((nums[i]%2 and nums[i+1]%2) or (nums[i]%2==0 and nums[i+1]%2==0)):
-                latest_hiccup=i
-        return [not x for x in result]
+        cnt=0
+        ps=[0]
+        for a, b in pairwise(nums):
+            if (a+b)%2:
+                cnt+=1
+            ps.append(cnt)
+        res=[]
+        for l, r in queries:
+            if ps[r]-ps[l]==r-l:
+                res.append(True)
+            else:
+                res.append(False)
+        return res
