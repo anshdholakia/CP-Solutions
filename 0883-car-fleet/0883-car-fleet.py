a7325ec1143d303d8218251afb4c9411b13a7a8b
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-02-11 15:51:13 -0500

    Time: 143 ms (83.31%), Space: 40.6 MB (21.52%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -3,7 +3,7 @@ class Solution:
         tt=[((target-p)/s, p) for p, s in zip(position, speed)]
         tt.sort(key=lambda x: x[1], reverse=True)
         stack=[]
-        for time, p in tt:
+        for time, pos in tt:
             while stack and stack[-1]>=time:
                 time=stack.pop()
             stack.append(time)
