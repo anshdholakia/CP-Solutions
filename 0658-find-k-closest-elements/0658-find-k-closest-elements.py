149f650fb09fd40b724bfcb3a9646b0d10d82107
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-02-24 15:13:22 -0500

    Time: 14 ms (41.09%), Space: 19 MB (82.91%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,29 +1,10 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        # using binary search first find the mid point
+        # use sliding window
         l, r = 0, len(arr)-1
-        while l<=r:
-            m=(l+r)//2
-            if arr[m]==x:
-                l=m-1
-                r=m+1
-                k-=1
-                break
-            elif arr[m]<x:
-                l=m+1
+        while r-l+1>k:
+            if abs(arr[l]-x)>abs(arr[r]-x):
+                l+=1
             else:
-                r=m-1
-        if r<l: l,r=r,l
-        # use two pointer approach to expand
-        while k:
-            if l==-1:
-                r+=1
-            elif r==len(arr):
-                l-=1
-            else:
-                if abs(arr[l]-x)<=abs(arr[r]-x):
-                    l-=1
-                else:
-                    r+=1
-            k-=1
-        return arr[l+1:r]
+                r-=1
+        return arr[l:r+1]
