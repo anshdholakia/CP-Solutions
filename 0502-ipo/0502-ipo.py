931f3ba4a742c06520b0822ba630d186438311f9
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-02-28 17:30:12 -0500

    Time: 268 ms (79.43%), Space: 42.2 MB (68.55%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,16 +1,22 @@
 class Solution:
-    def findMaximizedCapital(self, k: int, w: int, profit: List[int], capital: List[int]) -> int:
-        sorted_investments=sorted([(c, p) for p, c in zip(profit, capital)], key=lambda x:(x[0], x[1]))
-        maxheap=[]
-        idx=-1
-        cur_cap=w
-        for _ in range(k):
-            for i in range(idx+1, len(sorted_investments)):
-                if sorted_investments[i][0]>cur_cap:
-                    break
-                idx=i
-                heapq.heappush(maxheap, -sorted_investments[i][1])
-            if not maxheap:
-                continue
-            cur_cap-=heapq.heappop(maxheap)
-        return cur_cap
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        # we would like to minimize the capital req
+        # but maximize the profit
+        # since we aren't going to subtract from profit
+        # our current capital is always increasing
+        pc=[(c, p) for p, c in zip(profits, capital)]
+        pc.sort(key=lambda x: x[0])
+        idx=0
+        heap=[]
+        cur_k=0
+        while idx<len(pc) or heap:
+            while idx<len(pc) and pc[idx][0]<=w:
+                heapq.heappush(heap, -pc[idx][1])
+                idx+=1
+            if heap:
+                w-=heapq.heappop(heap)
+                cur_k+=1
+                if cur_k==k: break
+            else:
+                break
+        return w
