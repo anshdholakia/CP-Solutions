987894012dab43d2fbb31d7d28a288d7721271c9
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-03-01 17:28:52 -0500

    Time: 0 ms (100%), Space: 17.8 MB (74.41%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,32 +1,29 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        # make the relations in a hashmap
-        relations=defaultdict(dict)
-        for relation, value in zip(equations, values):
-            relations[relation[0]][relation[1]]=value
-            relations[relation[1]][relation[0]]=1/value
+        # construct the graph
+        G=defaultdict(dict)
+        for eq, val in zip(equations, values):
+            G[eq[0]][eq[1]]=val
+            G[eq[1]][eq[0]]=1/val
         visited=set({})
-        def find(x, target):
-            if x not in relations:
-                return inf
-            if x in visited:
-                return inf
-            if x==target:
-                return 1
-            visited.add(x)
-            for neighbor, value in relations[x].items():
-                if neighbor not in visited:
-                    res=find(neighbor, target)*value
-                    if res!=inf:
-                        visited.remove(x)
-                        return res
-            visited.remove(x)
-            return inf
+        def dfs(start, end):
+            if start==end:
+                return 1.0, True
+            visited.add(start)
+            res=-1.0
+            ret=False
+            for n, v in G[start].items():
+                if n not in visited:
+                    mult, rret = dfs(n, end)
+                    if rret:
+                        ret=True
+                        res=max(res, v*mult)
+            visited.remove(start)
+            return res, ret
         res=[]
-        for fromm, to in queries:
-            curres=find(fromm, to)
-            if curres==inf:
+        for q1, q2 in queries:
+            if q1 not in G or q2 not in G:
                 res.append(-1)
-            else:
-                res.append(curres)
+                continue
+            res.append(dfs(q1, q2)[0])
         return res
