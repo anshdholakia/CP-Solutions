44525de9e2941271274682002e21fa3ebee46ba2
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-03-02 16:23:14 -0500

    Time: 423 ms (65.38%), Space: 95.5 MB (35.01%) - LeetHub

--- 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.py
+++ 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.py
@@ -1,26 +1,23 @@
 class Solution:
     def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
-        # outgoing - incoming edges.
-        # euler's path
-        G_out=defaultdict(list)
-        G_in=defaultdict(list)
+        G=defaultdict(list)
+        edge_count=defaultdict(int)
         for u, v in pairs:
-            G_out[u].append(v)
-            G_in[v].append(u)
-        start=u
-        # find the node with the outgoing - incoming = 1
-        for node in G_out:
-            if len(G_out[node])-len(G_in[node])==1:
-                start=node
-                break
-        # do euler's path
+            G[u].append(v)
+            edge_count[u]+=1
+            edge_count[v]-=1
         res=[]
-        def dfs(node):
-            while G_out[node]:
-                dfs(G_out[node].pop())
-            res.append(node)
+        def dfs(n):
+            if not G[n]:
+                res.append(n)
+                return
+            while G[n]:
+                dfs(G[n].pop())
+            res.append(n)
+        start=pairs[0][0]
+        for k, v in edge_count.items():
+            if v==1:
+                start=k
         dfs(start)
-        res=res[::-1]
+        res.reverse()
         return [[res[i], res[i+1]] for i in range(len(res)-1)]
-
-        
