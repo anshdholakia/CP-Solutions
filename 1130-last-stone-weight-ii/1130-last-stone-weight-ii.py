214f6b88458212ad67d589e28324e761a0d2b174
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-03-04 17:57:01 -0500

    Time: 7 ms (85.32%), Space: 17.9 MB (75.49%) - LeetHub

--- 1130-last-stone-weight-ii/1130-last-stone-weight-ii.py
+++ 1130-last-stone-weight-ii/1130-last-stone-weight-ii.py
@@ -1,14 +1,11 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        # split this into piles close to the mid way of the stones
-        SUM=sum(stones)
-        HALF=SUM//2
         sums=set({0})
-        res=inf
         for s in stones:
-            for curs in sums.copy():
-                curs+=s
-                HALF1=SUM-curs
-                res=min(res, abs(HALF1-curs))
-                sums.add(curs)
+            for l in sums.copy():
+                sums.add(s+l)
+        res=inf
+        SUM=sum(stones)
+        for s in sums:
+            res=min(res, abs((SUM-s)-s))
         return res
