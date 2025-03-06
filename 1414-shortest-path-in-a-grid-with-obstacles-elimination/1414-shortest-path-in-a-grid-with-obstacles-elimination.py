c20dd4e5d1351467b6a66914b39e3b43e8472cac
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-03-05 19:13:45 -0500

    Time: 445 ms (31.94%), Space: 25.4 MB (38.93%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,20 +1,22 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        visited=set({(0, 0, k)})
-        queue=collections.deque([(0, 0, k, 0)])
-        level=-1
-        res=inf
+        # modified bfs
+        queue=collections.deque([(0, 0, 0)])
+        steps=0
+        visited=set({(0, 0, 0)})
         while queue:
-            level+=1
             for _ in range(len(queue)):
-                x, y, popk, popw = queue.popleft()
+                x, y, cur_k=queue.popleft()
                 if x==len(grid)-1 and y==len(grid[0])-1:
-                    return popw
-                for dx, dy in pairwise([-1,0,1,0,-1]):
-                    if 0<=dx+x<len(grid) and 0<=dy+y<len(grid[0]) and (dx+x, dy+y, popk) not in visited:
-                        visited.add((dx+x,dy+y,popk))
-                        if grid[dx+x][dy+y]==1 and popk:
-                            queue.append((dx+x, dy+y, popk-1, popw+1))
-                        elif grid[dx+x][dy+y]==0:
-                            queue.append((dx+x, dy+y, popk, popw+1))
+                    return steps
+                for dirx, diry in pairwise([-1,0,1,0,-1]):
+                    if 0<=x+dirx<len(grid) and 0<=y+diry<len(grid[0]) and (x+dirx, y+diry, cur_k) not in visited:
+                        if grid[x+dirx][y+diry]:
+                            if cur_k+1<=k:
+                                visited.add((x+dirx, y+diry, cur_k))
+                                queue.append((x+dirx, y+diry, cur_k+1))
+                        else:
+                            visited.add((x+dirx, y+diry, cur_k))
+                            queue.append((x+dirx, y+diry, cur_k))
+            steps+=1
         return -1
