f81d559db03291d6c91f98db41b15de59a5e0465
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-03-11 02:06:46 -0400

    Time: 35 ms (21.45%), Space: 18.6 MB (7.33%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
@@ -1,21 +1,44 @@
+class SegmentTree:
+    def __init__(self, sum, L, R):
+        self.sum, self.L, self.R = sum, L, R
+        self.left = None
+        self.right = None
+    @staticmethod
+    def build(nums, L, R):
+        if L==R:
+            return SegmentTree(nums[L], L, R)
+        M=(L+R)//2
+        node=SegmentTree(0, L, R)
+        node.left=SegmentTree.build(nums, L, M)
+        node.right=SegmentTree.build(nums, M+1, R)
+        node.sum=node.left.sum+node.right.sum
+        return node
+    def find_next_kth(self, k):
+        # this function finds the next kth element
+        if self.L==self.R:
+            return self.L
+        if self.left.sum>=k:
+            return self.left.find_next_kth(k)
+        return self.right.find_next_kth(k-self.left.sum)
+    def update(self, index, val):
+        if self.L==self.R:
+            self.sum=val
+            return
+        mid=(self.L+self.R)//2
+        if index>mid:
+            self.right.update(index, val)
+        else:
+            self.left.update(index, val)
+        self.sum=self.left.sum+self.right.sum
+
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        # sort the array based on comparator
-        def cmp(a, b):
-            if a[0]<b[0]:
-                return -1
-            elif a[0]==b[0]:
-                return -1 if a[1]<b[1] else 1
-            return 1
-        people.sort(key=cmp_to_key(cmp))
-        # for each of the element place it in the array
+        people.sort(key=lambda x: (x[0], -x[1]))
+        st=SegmentTree.build([1]*len(people), 0, len(people)-1)
         res=[None]*len(people)
         for h, p in people:
-            cnt=0
-            for i in range(len(res)):
-                if res[i]==None and cnt==p:
-                    res[i]=[h,p]
-                    break 
-                elif res[i]==None or res[i][0]>=h:
-                    cnt+=1
+            k=st.find_next_kth(p+1)
+            res[k]=[h, p]
+            st.update(k, 0)
         return res
+        
