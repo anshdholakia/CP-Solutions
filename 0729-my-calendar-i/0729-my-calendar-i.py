fc127b943ce3adfac0dc038617a7857b3cc134fc
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-03-11 13:57:04 -0400

    Time: 26 ms (95.94%), Space: 18.5 MB (98.1%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,20 +1,34 @@
+class TreeNode:
+    def __init__(self, start, end):
+        self.left, self.right = None, None
+        self.start, self.end = start, end
+
+    def insert(self, start, end):
+        if end<=self.start:
+            if not self.left:
+                self.left=TreeNode(start, end)
+                return True
+            return self.left.insert(start, end)
+        elif self.end<=start:
+            if not self.right:
+                self.right = TreeNode(start, end)
+                return True
+            return self.right.insert(start, end)
+        return False
+
 class MyCalendar:
 
     def __init__(self):
-        self.intervals = []
+        self.root=None
 
-    def book(self, start: int, end: int) -> bool:
-        i = 0
-        for i, (s, e) in enumerate(self.intervals):
-            if s >= end:
-                break
-            if e <= start:
-                continue
-            return False
-        index = bisect_left(self.intervals, end, 0, len(self.intervals), key=lambda x: x[0])
-        self.intervals.insert(index, (start, end))
-        return True
+    def book(self, startTime: int, endTime: int) -> bool:
+        if not self.root:
+            self.root=TreeNode(startTime, endTime)
+            return True
+        return self.root.insert(startTime, endTime)
+        
+
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
-# param_1 = obj.book(start,end)
+# param_1 = obj.book(startTime,endTime)
