78c847bfd860d9a07018ce387e1f9d3efe70d3dc
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-03-13 00:15:36 -0400

    Time: 75 ms (75.06%), Space: 38.2 MB (65.32%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.py
@@ -1,16 +1,18 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        SUM = sum(nums)
-        if not SUM%p:
+        ps={0:-1}
+        SUM=sum(nums)
+        if SUM%p==0:
             return 0
-        need = SUM%p
-        result = len(nums)
-        cur_sum = 0
-        ps = {0:-1} # key is cur_sum%p and value is index
+        CUR=0
+        REM=SUM%p
+        size=len(nums)
         for i, n in enumerate(nums):
-            cur_sum = (cur_sum + n)%p
-            new = (cur_sum - need + p)%p
-            if new in ps:
-                result=min(result, i-ps[new])
-            ps[cur_sum] = i
-        return -1 if result==len(nums) else result
+            CUR+=n
+            CUR_MOD=CUR%p
+            x=(CUR_MOD-REM)%p
+            # find x in ps
+            if x in ps:
+                size=min(size, i-ps[x])
+            ps[CUR_MOD]=i
+        return size if size!=len(nums) else -1
