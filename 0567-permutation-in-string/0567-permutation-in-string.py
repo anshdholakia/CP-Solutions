36db4984c530d54e9354b3773ffda8546e67e024
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-09 23:59:29 -0400

    Time: 35 ms (42.23%), Space: 18.1 MB (19.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,18 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        l = 0
-        target = collections.Counter(s1)
-        current = {}
+        target=collections.Counter(s1)
+        l=0
+        hashmap={}
         for r in range(len(s2)):
-            if s2[r] in target:
-                current[s2[r]] = current.get(s2[r], 0) + 1
-                while current[s2[r]]>target[s2[r]]:
-                    if s2[l] in target:
-                        current[s2[l]]-=1
-                    l+=1
-                if current==target:
-                    return True
-            else:
-                l=r+1
-                current={}
+            hashmap[s2[r]]=hashmap.get(s2[r],0)+1
+            while hashmap.get(s2[r],0)>target.get(s2[r],0):
+                hashmap[s2[l]]-=1
+                if not hashmap[s2[l]]:
+                    del hashmap[s2[l]]
+                l+=1
+            if hashmap==target:
+                return True
         return False
+
