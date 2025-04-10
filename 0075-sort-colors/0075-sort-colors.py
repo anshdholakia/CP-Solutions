a839b5acfb1d66302bd1e988ad8787e003844d7c
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-10 00:09:27 -0400

    Time: 0 ms (100%), Space: 17.9 MB (12.21%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,19 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # quick select algorithm
-        def quick_select(i, j, swp1, swp2):
-            while i<j:
-                while i<j and nums[i]!=swp2:
-                    i+=1
-                while i<j and nums[j]!=swp1:
-                    j-=1
-                if i<j:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    i+=1
-                    j-=1
-        quick_select(0, len(nums)-1, 0, 2)
-        quick_select(0, len(nums)-1, 0, 1)
-        quick_select(0, len(nums)-1, 1, 2)
-        
-            
+        # use quickselect
+        def quickselect(one_color, two_color):
+            l, r = 0, len(nums)-1
+            while l<r:
+                while l<r and nums[l]!=one_color:
+                    l+=1
+                while r>l and nums[r]!=two_color:
+                    r-=1
+                if l<r:
+                    nums[l], nums[r] = nums[r], nums[l]
+                    l+=1
+                    r-=1
+        quickselect(2, 0)
+        quickselect(1, 0)
+        quickselect(2, 1)
