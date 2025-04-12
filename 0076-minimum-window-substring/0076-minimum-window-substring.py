b988c0cc3db37e43c6810e621e81207a90598cb5
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-12 16:00:53 -0400

    Time: 127 ms (38.35%), Space: 18.5 MB (17.2%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,23 +1,22 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        l = 0
-        target = collections.Counter(t)
-        result = float("inf")
-        current = {}
-        result_str = ""
-        have, need = 0, len(target)
+        target=collections.Counter(t)
+        need=len(target)
+        have=0
+        result=""
+        res_len=inf
+        l=0
+        count={}
         for r in range(len(s)):
-            if s[r] in target:
-                current[s[r]] = current.get(s[r], 0) + 1
-                if current[s[r]]==target[s[r]]:
-                    have+=1
-                while have==need:
-                    if result>r-l+1:
-                        result=r-l+1
-                        result_str = s[l:r+1]
-                    if s[l] in target:
-                        current[s[l]]-=1
-                        if current[s[l]]<target[s[l]]:
-                            have-=1
-                    l+=1
-        return result_str
+            count[s[r]]=count.get(s[r],0)+1
+            if count[s[r]]==target[s[r]]:
+                have+=1
+            while have==need:
+                if r-l+1<res_len:
+                    res_len=r-l+1
+                    result=s[l:r+1]
+                count[s[l]]-=1
+                if s[l] in target and count[s[l]]<target[s[l]]:
+                    have-=1
+                l+=1
+        return result
