d026ac8b562909ff8e8a69c46e138bf18e18001c
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-13 20:28:29 -0400

    Time: 5438 ms (39.52%), Space: 35.6 MB (5.76%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
+        # treat each ballon as if you can pop it last
         @cache
         def dp(l, r):
             if l>r:
                 return 0
-            res=0
-            for k in range(l, r+1):
-                cur=(nums[l-1] if l>0 else 1)*nums[k]*(nums[r+1] if r+1<len(nums) else 1)
-                res=max(res, dp(l,k-1)+cur+dp(k+1, r))
+            res=-inf
+            for i in range(l, r+1):
+                res=max(res, dp(l, i-1)+((nums[l-1] if l-1>=0 else 1)*nums[i]*(nums[r+1] if r+1<len(nums) else 1))+dp(i+1, r))
             return res
         return dp(0, len(nums)-1)
