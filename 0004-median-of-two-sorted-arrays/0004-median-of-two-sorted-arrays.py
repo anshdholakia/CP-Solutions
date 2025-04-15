3b02b66d428c87a9a1d72f0f88108a4efa8e92a2
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-14 22:26:39 -0400

    Time: 2 ms (58.97%), Space: 18.2 MB (40.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,25 +1,27 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # use binary search
-        if not nums1 or not nums2:
-            nums1+=nums2
-            if len(nums1)%2:
-                return nums1[len(nums1)//2]
-            return (nums1[len(nums1)//2]+nums1[(len(nums1)//2)-1])/2
-        if len(nums1)>len(nums2):
+        if not nums2:
             nums1, nums2 = nums2, nums1
-        TOTAL=len(nums1)+len(nums2)
-        HALF=TOTAL//2
-        l, r = 0, len(nums1)-1
+        if not nums1:
+            if len(nums2)%2==0:
+                return (nums2[len(nums2)//2]+nums2[(len(nums2)//2)-1])/2
+            return nums2[len(nums2)//2]
+        if len(nums1)<len(nums2):
+            nums1, nums2 = nums2, nums1
+        l, r = 0, len(nums2)-1
+        total=len(nums1)+len(nums2)
+        half=total//2
         while True:
             m=(l+r)//2
-            LOWER1, UPPER1 = nums1[m] if 0<=m<len(nums1) else -inf, nums1[m+1] if 0<=m+1<len(nums1) else inf
-            LOWER2, UPPER2 = nums2[HALF-m-2] if 0<=HALF-m-2<len(nums2) else -inf, nums2[HALF-m-1] if 0<=HALF-m-1<len(nums2) else inf
-            if LOWER1<=UPPER2 and LOWER2<=UPPER1:
-                if TOTAL%2:
-                    return min(UPPER1, UPPER2)
-                return (min(UPPER1, UPPER2)+max(LOWER1, LOWER2))/2
-            if LOWER1>UPPER2:
+            L2=nums2[m] if 0<=m<len(nums2) else -inf
+            R2=nums2[m+1] if 0<=m+1<len(nums2) else inf
+            L1=nums1[half-m-2] if 0<=half-m-2<len(nums1) else -inf
+            R1=nums1[half-m-1] if 0<=half-m-1<len(nums1) else inf
+            if L2<=R1 and L1<=R2:
+                if total%2==0:
+                    return (max(L1, L2)+min(R1, R2))/2
+                return min(R1, R2)
+            if L2>R1:
                 r=m-1
             else:
                 l=m+1
