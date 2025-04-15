fc41e6cacb8a63ce58bd06e40b90cbd39580a4ab
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-14 22:59:34 -0400

    Time: 0 ms (100%), Space: 24.5 MB (99.95%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -3,9 +3,9 @@ class Solution:
         l, r = 0, len(nums)-1
         while l<=r:
             m=(l+r)//2
-            if (nums[m-1] if m>0 else -inf)!=nums[m]!=(nums[m+1] if m<len(nums)-1 else -inf):
+            if (nums[m-1] if m>0 else inf)!=nums[m]!=(nums[m+1] if m+1<len(nums) else inf):
                 return nums[m]
-            leftSize=m-1 if m>0 and nums[m-1]==nums[m] else m
+            leftSize=m-1 if (m>0 and nums[m-1]==nums[m]) else m
             if leftSize%2:
                 r=m-1
             else:
