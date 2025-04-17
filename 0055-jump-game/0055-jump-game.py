5f2b531a494d5fd62112cd2fdef5cd0679316d7c
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-16 21:38:02 -0400

    Time: 31 ms (49.54%), Space: 18.5 MB (83.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        res=True
-        minimum=len(nums)-1
+        min_idx=len(nums)-1
         for i in range(len(nums)-2, -1, -1):
-            if nums[i]+i>=minimum:
-                minimum=i
-                res=True
-            else:
-                res=False
-        return res
+            if nums[i]+i>=min_idx:
+                min_idx=min(min_idx, i)
+        return min_idx==0
