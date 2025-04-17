2ad9c74f2ea28721dc909c1fbc975ce7da9e6ec4
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-16 21:16:35 -0400

    Time: 11 ms (74.85%), Space: 18.5 MB (15.88%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,25 +1,22 @@
-from copy import deepcopy
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        result = []
-        board = [['.' for _ in range(n)] for _ in range(n)]
-        cols, ldiag, rdiag = set({}), set({}), set({})
+        result=[]
+        board=[['.']*n for _ in range(n)]
+        ldiag, rdiag, column = set({}), set({}), set({})
         def backtrack(row):
             if row==n:
                 result.append(["".join(x) for x in board])
-            for c in range(n):
-                if c not in cols and c+row not in rdiag and c-row not in ldiag:
-                    cols.add(c)
-                    rdiag.add(c+row)
-                    ldiag.add(c-row)
-                    board[row][c] = 'Q'
+                return
+            for col in range(n):
+                if row+col not in ldiag and row-col not in rdiag and col not in column:
+                    column.add(col)
+                    ldiag.add(row+col)
+                    rdiag.add(row-col)
+                    board[row][col]='Q'
                     backtrack(row+1)
-                    board[row][c] = '.'
-                    cols.remove(c)
-                    rdiag.remove(c+row)
-                    ldiag.remove(c-row)
-                    
-
-
+                    board[row][col]='.'
+                    column.remove(col)
+                    ldiag.remove(row+col)
+                    rdiag.remove(row-col)
         backtrack(0)
         return result
