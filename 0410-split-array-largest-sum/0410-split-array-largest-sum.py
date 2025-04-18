02b999e7d043a90a0145bf089e21ee7df758cdec
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-17 22:20:35 -0400

    Time: 4 ms (32.3%), Space: 17.9 MB (59.72%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,24 +1,24 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        def check(x):
-            # check if you can make subarray under k with max sum of x
-            cur_sum=0
-            cur_k=1
-            for n in nums:
-                cur_sum+=n
-                if cur_sum>x:
-                    cur_sum=n
-                    if cur_sum>x:
-                        return False
-                    cur_k+=1
-            return cur_k<=k
-        l, r = max(nums), sum(nums)
-        res=-1
+        total=sum(nums)
+        l, r = 0, total
+        ans=0
+        def check(mid):
+            curs=0
+            array=1
+            for i in range(len(nums)):
+                curs+=nums[i]
+                if nums[i]>mid:
+                    return False
+                if curs>mid:
+                    curs=nums[i]
+                    array+=1
+            return array<=k
         while l<=r:
             m=(l+r)//2
             if check(m):
-                res=m
+                ans=m
                 r=m-1
             else:
                 l=m+1
-        return res
+        return ans
