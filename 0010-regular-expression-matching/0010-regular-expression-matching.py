786690d47a35db4d1350a52b5e9629b7bbffefe5
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-17 23:01:19 -0400

    Time: 7 ms (69.26%), Space: 18 MB (29.66%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -2,20 +2,18 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         @cache
         def dp(i, j):
-            if i>=len(s) and j>=len(p):
-                return True
             if j>=len(p):
-                return False
+                return i>=len(s)
             if j+1<len(p) and p[j+1]=='*':
-                # you can either include p[j] or not include it
-                # removing it
-                res1=dp(i,j+2)
-                # including it
+                # either take * or skip it
+                # skip
+                res1=dp(i, j+2)
+                # take
                 res2=False
                 if i<len(s) and (s[i]==p[j] or p[j]=='.'):
-                    res2=dp(i+1,j)
+                    res2=dp(i+1, j)
                 return res1 or res2
-            if i<len(s) and (s[i]==p[j] or p[j]=='.'):
-                return dp(i+1,j+1)
+            elif i<len(s) and (s[i]==p[j] or p[j]=='.'):
+                return dp(i+1, j+1)
             return False
         return dp(0, 0)
