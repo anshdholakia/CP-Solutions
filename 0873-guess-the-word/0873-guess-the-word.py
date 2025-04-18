90bdb500450cab4f1819272e383fd507fde029a1
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-17 21:21:13 -0400

    Time: 40 ms (68.95%), Space: 18 MB (24%) - LeetHub

--- 0873-guess-the-word/0873-guess-the-word.py
+++ 0873-guess-the-word/0873-guess-the-word.py
@@ -7,34 +7,26 @@
 
 class Solution:
     def findSecretWord(self, words: List[str], master: 'Master') -> None:
-        # sort the words based on their positional uniqueness
-        place_count=defaultdict(int)
-        for word in words:
-            for i, c in enumerate(word):
-                place_count[str(i)+c]+=1
-        def score_word(word):
-            return sum([place_count[str(i)+c] for i, c in enumerate(word)])
-        # sorted from least common to most common i.e. most unique to least unique
-        words.sort(key=score_word)
-        def get_diff(word1, word2):
-            if word1==word2:
-                return 0
-            score=0
-            for a, b in zip(word1, word2):
-                if a==b:
-                    score+=1
-            return score
-
-        end=-1
-        for _ in range(30):
-            chosen_word=words[end]
-            score=master.guess(chosen_word)
-            if score==6:
-                break
-            elif score==0:
-                # filter out words which do not marginally match with this
-                words=[word for word in words if not any(a==b for a, b in zip(word, chosen_word))]
+        while True:
+            word=random.choice(words)
+            guess=master.guess(word)
+            new_words=[]
+            if guess==0:
+                # remove all words which match with this word
+                for w in words:
+                    for i, c in enumerate(w):
+                        if c==word[i]:
+                            break
+                    else:
+                        new_words.append(w)
+            elif guess==6:
+                return
             else:
-                # the secret word would have the same number of score with the chosen_word
-                words=[word for word in words if get_diff(word, chosen_word)==score]
-
+                for w in words:
+                    cnt=0
+                    for i, c in enumerate(w):
+                        if c==word[i]:
+                            cnt+=1
+                    if cnt==guess:
+                        new_words.append(w)
+            words=new_words
