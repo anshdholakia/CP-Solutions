ca511e91918011ef3022750277eb1a080f7135e7
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-20 18:27:33 -0400

    Time: 0 ms (100%), Space: 23.4 MB (12.71%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,30 +8,26 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # split the head and put in one by one
+        # split the list
         slow, fast = head, head.next
         while fast and fast.next:
-            slow=slow.next
             fast=fast.next.next
-        head2=slow.next
-        if not head2:
-            return
+            slow=slow.next
+        half=slow.next
         slow.next=None
-        # reverse this head2
-        a, b = head2, head2.next
+        if not half:
+            return
+        # reverse half
+        a, b = half, half.next
         a.next=None
         while b:
             c=b.next
             b.next=a
             a=b
             b=c
-        head2=a
-        ptr1, ptr2 = head, head2
+        ptr1, ptr2 = head, a
         while ptr1 and ptr2:
-            temp_behind=ptr2.next
-            temp=ptr1.next
+            temp1, temp2 = ptr1.next, ptr2.next
             ptr1.next=ptr2
-            ptr2.next=temp
-            ptr1=temp
-            ptr2=temp_behind
-        
+            ptr2.next=temp1
+            ptr1, ptr2 = temp1, temp2
