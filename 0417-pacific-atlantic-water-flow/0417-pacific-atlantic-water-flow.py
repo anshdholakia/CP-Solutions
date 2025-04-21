003f446148b3bbdc0ec46d29c8b56f51154d4358
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-21 01:14:48 -0400

    Time: 55 ms (21.37%), Space: 19.3 MB (80.45%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,24 +1,16 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific, atlantic = set({}), set({})
-        for i in range(len(heights)):
-            for j in range(len(heights[0])):
-                if i==0 or j==0:
-                    pacific.add((i, j))
-                if i==len(heights)-1 or j==len(heights[0])-1:
-                    atlantic.add((i, j))
-        def dfs(x, y, mapping):
-            queue = collections.deque([(x, y)])
-            while queue:
-                popx, popy = queue.popleft()
-                for dirx, diry in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    if popx+dirx>=0 and popx+dirx<len(heights) and popy+diry>=0 and popy+diry<len(heights[0]) and (popx+dirx, popy+diry) not in mapping and heights[popx+dirx][popy+diry]>=heights[popx][popy]:
-                        queue.append((popx+dirx, popy+diry))
-                        mapping.add((popx+dirx, popy+diry))
+        atlantic=set({})
+        pacific=set({})
+        def dfs(i, j, hashset):
+            hashset.add((i, j))
+            for dx, dy in pairwise([-1,0,1,0,-1]):
+                if 0<=dx+i<len(heights) and 0<=dy+j<len(heights[0]) and (dx+i, dy+j) not in hashset and heights[dx+i][dy+j]>=heights[i][j]:
+                    dfs(dx+i,dy+j,hashset)
         for i in range(len(heights)):
             for j in range(len(heights[0])):
                 if i==0 or j==0:
                     dfs(i, j, pacific)
                 if i==len(heights)-1 or j==len(heights[0])-1:
                     dfs(i, j, atlantic)
-        return list(pacific.intersection(atlantic))
+        return list(atlantic.intersection(pacific))
