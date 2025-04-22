c99274fccee3e329efb4183f9576ea0010877197
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-21 21:12:46 -0400

    Time: 3 ms (75.61%), Space: 18.4 MB (20.41%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,20 +1,15 @@
-from sortedcontainers import SortedList
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        tickets.sort()
-        G = defaultdict(lambda: SortedList())
-        for fro, to in tickets:
-            G[fro].add(to)
-        visited=[]
+        # eulerian path
+        G=defaultdict(list)
+        for u, v in tickets:
+            G[u].append(v)
+        for k in G:
+            G[k].sort(reverse=True)
+        res=[]
         def dfs(airport):
-            if len(visited)==len(tickets):
-                visited.append(airport)
-                return True
-            for n in G[airport]:
-                visited.append(airport)
-                G[airport].remove(n)
-                if dfs(n):
-                    return visited
-                G[airport].add(n)
-                visited.pop()
-        return dfs("JFK")
+            while G[airport]:
+                dfs(G[airport].pop())
+            res.append(airport)
+        dfs("JFK")
+        return res[::-1]
