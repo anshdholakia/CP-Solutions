df928cc5d2b19cb6dd2a241ad365ad9a04a7bd99
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-21 21:53:36 -0400

    Time: 27 ms (65.71%), Space: 18.6 MB (24.53%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -1,13 +1,15 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        heap=[(grid[0][0], 0, 0)]
-        visited=set()
-        visited.add((0, 0))
-        while heap:
-            val, x, y = heapq.heappop(heap)
+        minheap=[(grid[0][0], 0, 0)]
+        visited=set({})
+        max_val=-inf
+        while minheap:
+            d, x, y = heapq.heappop(minheap)
+            max_val=max(max_val, grid[x][y])
             if x==len(grid)-1 and y==len(grid[0])-1:
-                return val
-            for dirx, diry in pairwise([-1,0,1,0,-1]):
-                if 0<=x+dirx<len(grid) and 0<=y+diry<len(grid[0]) and (x+dirx, y+diry) not in visited:
-                    visited.add((x+dirx, y+diry))
-                    heapq.heappush(heap, (max(val, grid[x+dirx][y+diry]), x+dirx, y+diry))
+                return max_val
+            visited.add((x, y))
+            for dx, dy in pairwise([-1,0,1,0,-1]):
+                if 0<=dx+x<len(grid) and 0<=dy+y<len(grid[0]) and (dx+x, y+dy) not in visited:
+                    visited.add((dx+x, dy+y))
+                    heapq.heappush(minheap, (grid[dx+x][y+dy], dx+x, dy+y))
