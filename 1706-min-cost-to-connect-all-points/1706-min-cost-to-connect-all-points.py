157e9d514e5862be1fc370cb3466abe9d88ba2fe
Ansh Dholakia <64213853+anshdholakia@users.noreply.github.com>
2025-04-21 21:46:45 -0400

    Time: 1236 ms (54.17%), Space: 92 MB (50.15%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -1,15 +1,33 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        init=tuple(points[0])
-        points=set([tuple(x) for x in points])
-        heap=[(0, init[0], init[1])]
-        visited=set({})
+        # use prims
+        edges=[]
+        for p1 in range(len(points)):
+            for p2 in range(p1+1, len(points)):
+                x, y = points[p1]
+                p, q = points[p2]
+                edges.append((abs(x-p)+abs(y-q), p1, p2))
+        edges.sort(key=lambda x: x[0])
+        parent=list(range(len(points)))
+        rank=[1]*len(points)
+        def find(n):
+            while n!=parent[n]:
+                parent[n]=parent[parent[n]]
+                n=parent[n]
+            return n
+        def union(a, b):
+            p1, p2 = find(a), find(b)
+            if p1==p2:
+                return False
+            if rank[p1]>rank[p2]:
+                parent[p2]=p1
+                rank[p1]+=rank[p2]
+            else:
+                parent[p1]=p2
+                rank[p2]+=rank[p1]
+            return True
         res=0
-        while heap:
-            v, x, y = heapq.heappop(heap)
-            if (x, y) in points:
-                res+=v
-                points.remove((x, y))
-                for dirx, diry in points:
-                    heapq.heappush(heap, (abs(x-dirx)+abs(y-diry), dirx, diry))
+        for d, u, v in edges:
+            if union(u, v):
+                res+=d
         return res
